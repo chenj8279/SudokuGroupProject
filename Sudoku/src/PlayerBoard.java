ca186dcc9169ae42c2fd455cc53cf93b13217d62
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-12-20 12:35:27 -0800

    Update PlayerBoard.java

--- Sudoku/src/PlayerBoard.java
+++ Sudoku/src/PlayerBoard.java
@@ -5,17 +5,4 @@ public class PlayerBoard extends Board{
 		super();
 	}
 	
-	public void addHints(Key k) {
-		for(int i = 0; i < 30; i++) {
-			int row = (int) Math.random()*super.getLenRow();
-			int col = (int) Math.random()*super.getLenCol();
-			
-			while(getNum(row, col) != 0) {
-				row = (int) Math.random()*super.getLenRow();
-				col = (int) Math.random()*super.getLenCol();
-			}
-			super.setNum(k.getNum(row, col), row, col);
-		}
-	}
-	
 }
