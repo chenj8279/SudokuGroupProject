f34eebc2fac41184112754392988a4049b24aae9
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-12-20 12:41:52 -0800

    Update PlayerBoard.java

--- Sudoku/src/PlayerBoard.java
+++ Sudoku/src/PlayerBoard.java
@@ -5,4 +5,17 @@ public class PlayerBoard extends Board{
 		super();
 	}
 	
+	public PlayerBoard(Key k) {
+		super();
+		for(int i = 0; i < 30; i++) {
+			int row = (int) Math.random()*super.getLenRow();
+			int col = (int) Math.random()*super.getLenCol();
+			
+			while(super.getNum(row, col) != 0) {
+				row = (int) Math.random()*super.getLenRow();
+				col = (int) Math.random()*super.getLenCol();
+			}
+			super.setNum(k.getNum(row, col), row, col);
+		}
+	}
 }
